1831e30498e5daacc12c10473bc7f68461015e5b
HuangChunHsu <51147856+HuangChunHsu@users.noreply.github.com>
2019-07-11 14:19:22 +0800

    Add files via upload

--- Week2/task1.R
+++ Week2/task1.R
@@ -13,11 +13,12 @@ data5<-aggregate(data2$suicides_num, by=list(Category=data2$year), FUN=sum)/aggr
 data1
 final<-data1%>%mutate(suicide_rate=data5$x)
 colnames(final)=c("year","Unemployment_Rate","GDP","Inflation","suicide_rate")
+fct_drop
 final
 ggplot(data = final, aes(x = year, y=suicide_rate)) +
   geom_point()
 ggplot(data = final, aes(x = Unemployment_Rate,)) +
-  geom_point(aes(y=suicide_rate ))+theme(axis.text.x= element_text(size=7, family="myFont", color="black", face= "bold", vjust=0.5, hjust=0.5))
+  geom_point(aes(y=suicide_rate ))+theme(axis.text.x= element_text(size=10, family="myFont", color="black", face= "bold", vjust=0.5, hjust=0.5))
 final %>%
   ggplot(aes(x=Unemployment_Rate, y=suicide_rate)) +
   geom_point(aes(col=Unemployment_Rate), position=position_jitter(width=.05)) +
